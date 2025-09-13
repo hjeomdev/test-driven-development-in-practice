65d6866d5e44cf721dd55526de3eaf7d22a68fd5
hjeom.dev <hjeom.dev@gmail.com>
2025-09-13 15:15:34 +0900

    Update - maven : 암호 검사기 테스트 코드 작성

---
+++ test-driven-develop-maven/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,35 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordStrengthMeterTest {
+
+    @Test
+    void meetsAllCriteria_Then_Strong() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG, result);
+
+        PasswordStrength result2 = meter.meter("abc1!@Add");
+        assertEquals(PasswordStrength.STRONG, result2);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@A");
+        assertEquals(PasswordStrength.NORMAL, result);
+
+        PasswordStrength result2 = meter.meter("Ab12!@A");
+        assertEquals(PasswordStrength.NORMAL, result2);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab!@ABqwer");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
+}
