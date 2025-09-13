b5256d2367e4133151b6f8adc703743248e46d50
hjeom.dev <hjeom.dev@gmail.com>
2025-09-13 15:16:47 +0900

    Update - maven : 암호 검사기 테스트 - 객체 생성 중복 제거

--- test-driven-develop-maven/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ test-driven-develop-maven/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -6,9 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordStrengthMeterTest {
 
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
     @Test
     void meetsAllCriteria_Then_Strong() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab12!@AB");
         assertEquals(PasswordStrength.STRONG, result);
 
@@ -18,7 +19,6 @@ public class PasswordStrengthMeterTest {
 
     @Test
     void meetsOtherCriteria_except_for_Length_Then_Normal() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab12!@A");
         assertEquals(PasswordStrength.NORMAL, result);
 
@@ -28,7 +28,6 @@ public class PasswordStrengthMeterTest {
 
     @Test
     void meetsOtherCriteria_except_for_number_Then_Normal() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab!@ABqwer");
         assertEquals(PasswordStrength.NORMAL, result);
     }
